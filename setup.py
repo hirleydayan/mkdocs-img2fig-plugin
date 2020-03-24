efec7cb5e39bd6ef58d60923c26baf7c0dc27df1
Hirley Dayan <hirleydayan@gmail.com>
2020-03-24 09:29:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(fname):
 
 setup(
     name='mkdocs-img2fig-plugin',
-    version='0.9.2',
+    version='0.9.3',
     description='A MkDocs plugin that converts markdown encoded images into <figure> elements.',
 	long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
